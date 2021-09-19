3f9154b3ce1e092d439d78210cf2d4241c38257a
Nancy <nzzuo@andrew.cmu.edu>
2021-09-18 19:22:02 -0500

    added whale png

--- src/components/NavbarElements.js
+++ src/components/NavbarElements.js
@@ -14,7 +14,7 @@ z-index: 12;
 `;
 
 export const NavLink = styled(Link)`
-color: #808080;
+color: #FFFFFF;
 display: flex;
 align-items: center;
 text-decoration: none;
@@ -28,7 +28,7 @@ cursor: pointer;
 
 export const Bars = styled(FaBars)`
 display: none;
-color: #808080;
+color: #FFFFFF;
 @media screen and (max-width: 768px) {
 	display: block;
 	position: absolute;
